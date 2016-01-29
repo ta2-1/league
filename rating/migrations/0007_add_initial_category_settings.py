e42b9df0857a2338546c99f35f5c618a084019a6
Taras Semenenko <taras.semenenko@gmail.com>
2016-01-30 00:14:34 +0300

    missed migrations

---
+++ rating/migrations/0007_add_initial_category_settings.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        orm.CategorySettings.objects.create(
+            last_tournaments_count=10,
+            max_competitors_count=32,
+            result_tournaments_count=4,
+            title=u'Параметры по умолчанию'
+        )
+
+    models = {
+        'rating.category': {
+            'Meta': {'ordering': "('position',)", 'object_name': 'Category'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'show_on_main': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '25'}),
+            'title_en': ('django.db.models.fields.CharField', [], {'max_length': '25', 'null': 'True', 'blank': 'True'}),
+            'title_ru': ('django.db.models.fields.CharField', [], {'max_length': '25', 'null': 'True', 'blank': 'True'})
+        },
+        'rating.categorysettings': {
+            'Meta': {'object_name': 'CategorySettings'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_tournaments_count': ('django.db.models.fields.PositiveIntegerField', [], {'default': '8'}),
+            'max_competitors_count': ('django.db.models.fields.PositiveIntegerField', [], {'default': '32'}),
+            'result_tournaments_count': ('django.db.models.fields.PositiveIntegerField', [], {'default': '4'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'rating.competitor': {
+            'Meta': {'object_name': 'Competitor'},
+            'birthDate': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'categories': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'competitors'", 'symmetrical': 'False', 'to': "orm['rating.Category']"}),
+            'firstName': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'firstName_en': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
+            'firstName_ru': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lastName': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'lastName_en': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
+            'lastName_ru': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'})
+        },
+        'rating.location': {
+            'Meta': {'object_name': 'Location'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'address_en': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
+            'address_ru': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'latitude': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'longitude': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'title_en': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
+            'title_ru': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'})
+        },
+        'rating.results': {
+            'Meta': {'object_name': 'Results'},
+            'competitor': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['rating.Competitor']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'place': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'resultset': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['rating.ResultSet']"})
+        },
+        'rating.resultset': {
+            'Meta': {'object_name': 'ResultSet'},
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['rating.Category']"}),
+            'competitors': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'resultsets'", 'symmetrical': 'False', 'through': "orm['rating.Results']", 'to': "orm['rating.Competitor']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'locations': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'resultsets'", 'symmetrical': 'False', 'to': "orm['rating.Location']"}),
+            'tournament': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['rating.Tournament']"})
+        },
+        'rating.rule': {
+            'Meta': {'object_name': 'Rule'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'text': ('django.db.models.fields.TextField', [], {'default': "''"}),
+            'text_en': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'text_ru': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'})
+        },
+        'rating.tournament': {
+            'Meta': {'object_name': 'Tournament'},
+            'end_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_past': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'squashclub_url': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'start_date': ('django.db.models.fields.DateField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'title_en': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
+            'title_ru': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['rating']
+    symmetrical = True
