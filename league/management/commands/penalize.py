9c06f7ae7c49eec06d0a843e9504e0fe7c32b4e5
Taras Semenenko <taras.semenenko@gmail.com>
2016-05-04 14:22:26 +0000

    Recreate rating including penalties

--- league/management/commands/penalize.py
+++ league/management/commands/penalize.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from optparse import make_option
 from datetime import datetime, timedelta, time
 from dateutil.relativedelta import relativedelta
 
@@ -8,20 +9,32 @@ from django.core.management.base import BaseCommand, NoArgsCommand
 
 from league.models import get_current_league, League, LeagueCompetitor, Rating
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
     help = "Penalize competitors who does not play much."
+    shared_option_list = (
+        make_option('--month', dest='month', help='past month for penalties'),
+    )
 
-    def handle_noargs(self, **options):
-        l = get_current_league()
+    option_list = BaseCommand.option_list + shared_option_list
+    current_league_id = 1007
+
+    def handle(self, *args, **options):
+        l = League.objects.get(id=self.current_league_id)
         lcc = LeagueCompetitor.objects.filter(league=l)
-        
+
         league_dt = datetime.combine(l.start_date, time()) \
                             .replace(hour=0, minute=0, second=0)
 
-        dt = datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0)
-        start_dt = dt - relativedelta(months=1)
-        end_dt = dt - timedelta(seconds=1)
-        
+        month = options.get('month', None)
+        if month is not None:
+            start_dt = datetime.strptime(month, '%Y-%m').replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+            dt = start_dt + relativedelta(months=1)
+            end_dt = dt - timedelta(seconds=1)
+        else:
+            dt = datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+            start_dt = dt - relativedelta(months=1)
+            end_dt = dt - timedelta(seconds=1)
+
         for lc in lcc:
             #print "----"
             #print lc
@@ -45,10 +58,10 @@ class Command(NoArgsCommand):
                     penalty = -5
                 else:
                     penalty = 0
-            
+
             if penalty < 0 and Rating.objects.filter(type='penalty', datetime=dt, player=lc.competitor).count() == 0:
                 r = Rating(league=l, type='penalty', datetime=dt, player=lc.competitor,
                            delta=penalty, rating_before=lc.saved_rating(datetime=end_dt))
                 print r
                 r.save()
-        
+
--- league/management/commands/recreate_rating.py
+++ league/management/commands/recreate_rating.py
@@ -1,24 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from optparse import make_option
+
 from datetime import datetime, timedelta, time
 from dateutil.relativedelta import relativedelta
 
+from django.core.management import call_command
 from django.core.management.base import BaseCommand, NoArgsCommand
 
-from league.models import League, LeagueCompetitor, Game, Rating 
-from league.utils import league_get_N, league_get_DELTA 
+from league.models import League, LeagueCompetitor, Game, Rating
+from league.utils import league_get_N, league_get_DELTA
 
 
 class Command(NoArgsCommand):
     help = "Recalculate league."
+    shared_option_list = (
+        make_option('--end', action='store_true', dest='is_finished',
+                    help='Flag if league is finished'),
+    )
+    option_list = NoArgsCommand.option_list + shared_option_list
 
     def handle_noargs(self, **options):
         league_id = 1007
-
+        first_penalty_month = 3
+        is_finished = options.get('is_finished', False)
         league = League.objects.get(id=league_id)
         Rating.objects.filter(league=league).delete()
+        saved_month = league.start_date.strftime('%Y-%m')
+        month_number = 1
         for game in Game.objects.filter(league=league).order_by('end_datetime'):
+            month = game.end_datetime.strftime('%Y-%m')
+            if month != saved_month:
+                month_number += 1
+                if month_number > first_penalty_month:
+                    call_command('penalize', month=saved_month)
+                saved_month = month
             game.save()
             print u"%s" % game
+        if is_finished:
+            call_command('penalize', '--month=%s' % saved_month)
 
